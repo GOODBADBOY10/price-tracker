6dbcbf1cf0792250607a41bc654ea51706fbebc7
Goodbadboy <selfmanademola@gmail.com>
2025-11-15 23:21:08 +0100

    Update dependencies and clean up target files
    - Added new dependencies: axum, tokio, reqwest, serde, serde_json, and chrono to Cargo.toml.
    - Removed various target files and directories to clean up the workspace.
    - Deleted unnecessary cached files and build artifacts to ensure a fresh build environment.

--- worker/target/debug/deps/worker-8cf1e89f2e2931c1.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/USER/dev-stuff/rust/price-tracker/worker/target/debug/deps/worker-8cf1e89f2e2931c1.d: src/main.rs
-
-/mnt/c/Users/USER/dev-stuff/rust/price-tracker/worker/target/debug/deps/libworker-8cf1e89f2e2931c1.rmeta: src/main.rs
-
-src/main.rs:
--- worker/target/debug/deps/worker-976b9cdae88404b6.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/USER/dev-stuff/rust/price-tracker/worker/target/debug/deps/worker-976b9cdae88404b6.d: src/main.rs
-
-/mnt/c/Users/USER/dev-stuff/rust/price-tracker/worker/target/debug/deps/libworker-976b9cdae88404b6.rmeta: src/main.rs
-
-src/main.rs:
